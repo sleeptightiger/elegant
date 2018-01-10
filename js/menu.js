0b7f66dd4a74cf85b90b3e5af1dab587c40dc349
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-01-10 13:05:35 -0600

    add hidden menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.hamburger');
+const hiddenMenu = document.querySelector('.hidden-menu');
 menu.addEventListener('click', () => {
   hamburger.classList.toggle('close');
+  hiddenMenu.classList.toggle('unhide');
 });
